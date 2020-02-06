4de792dc2df4176b6aae27054c7fc40494bd3633
chrismckie19 <chrismckie19@me.com>
2020-02-05 17:15:06 -0800

    I added a period at the end

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,9 +1,18 @@
 class HelloWorld {
-test 
 
- constructor(test
-)   
-greet() [
-    console.log (Chris McKie)
-]
+    firstName = '';
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayHello(){
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName + '.');
+    }
+
 }
+
+const myHelloClassInstance = new HelloWorld('Chris', 'McKie');
+myHelloClassInstance.sayHello();
